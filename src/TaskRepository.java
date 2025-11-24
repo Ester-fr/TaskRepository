0980b885cd6893ed4edb4fb1d700e88c77aeedfa
Ester-Fr <sarafrankb@gmail.com>
2025-11-24 17:25:52 +0200

    Implement core CRUD operations in TaskRepository

--- src/TaskRepository.java
+++ src/TaskRepository.java
@@ -42,4 +42,56 @@ public class TaskRepository {
         JsonUtils.save(rawList, filePath);
     }
 
+    public void add(Task task) {
+        tasks.add(task);
+        save();
+    }
+
+    public boolean update(int id, String field, String newValue) {
+        for (Task t : tasks) {
+            if (t.getId() == id) {
+                switch (field.toLowerCase()) {
+                    case "title":
+                        t.setTitle(newValue);
+                        break;
+                    case "description":
+                        t.setDescription(newValue);
+                        break;
+                    case "status":
+                        t.setStatus(Status.valueOf(newValue.toUpperCase()));
+                        break;
+                    default:
+                        return false;
+                }
+                save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean delete(int id) {
+        Iterator<Task> it = tasks.iterator();
+        while (it.hasNext()) {
+            if (it.next().getId() == id) {
+                it.remove();
+                save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Task getById(int id) {
+        for (Task t : tasks) {
+            if (t.getId() == id)
+                return t;
+        }
+        return null;
+    }
+
+    public List<Task> listAll() {
+        return new ArrayList<>(tasks);
+    }
 }
