72970f2614795fe7f9e36d23e1800176eb0af270
Ester-Fr <sarafrankb@gmail.com>
2025-11-24 12:59:02 +0200

    Create Task class with attributes, constructor, and Status enum for task state management

---
+++ src/Task.java
@@ -0,0 +1,61 @@
+
+enum Status {
+    NEW, IN_PROGRESS, DONE
+}
+
+public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private Status status;
+
+    public Task(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = Status.NEW;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
